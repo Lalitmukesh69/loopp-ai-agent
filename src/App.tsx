7be5c4ecadef21f0d258e32785ad0a3f1cbe8b66
Lalit Mukesh <ashsaxena86@gmail.com>
2026-05-11 18:07:31 +0530

    fix: revert to BrowserRouter and add vercel.json for SPA routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { RecordingProvider } from "@/contexts/RecordingContext";
 import Index from "./pages/marketing/Index";
 import Privacy from "./pages/marketing/Privacy";
@@ -47,7 +47,7 @@ const App = () => (
       <RecordingProvider>
         <Toaster />
         <Sonner />
-        <HashRouter>
+        <BrowserRouter>
           <ScrollToTop />
           <Routes>
             <Route path="/" element={<Index />} />
@@ -93,7 +93,7 @@ const App = () => (
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </HashRouter>
+        </BrowserRouter>
       </RecordingProvider>
     </TooltipProvider>
   </QueryClientProvider>
